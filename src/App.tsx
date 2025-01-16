834368ee172c1ed65619bd7a190b5df97667a78a
leedbswo107 <ljyu1@naver.com>
2025-01-16 23:26:16 +0900

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>test</h1>
+      <div>안녕</div>
+    </div>
+  );
+};
+
+export default App;
