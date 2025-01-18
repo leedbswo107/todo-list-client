f7d3a81cbf5cfc3b27bd0ef7997a6c9e335823d4
leedbswo107 <ljyu1@naver.com>
2025-01-18 23:59:43 +0900

    feat: test hooks

---
+++ src/hooks/useModeSwitch.ts
@@ -0,0 +1,9 @@
+// import React, { useState } from 'react';
+
+// const useModeSwitch = (value: boolean) => {
+//   const [mode, setMode] = useState(value);
+//   setMode(!mode);
+//   return mode;
+// };
+
+// export default useModeSwitch;
