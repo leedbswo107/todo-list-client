e50cf173418cac6d856eee822b4abe5b2f99fac1
leedbswo107 <ljyu1@naver.com>
2025-01-20 23:51:26 +0900

    feat: ui test

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './App.css';
 import FloatingAddBtn from 'components/FloatingAddBtn';
 import Header from 'components/Header';
-import NightModeToggleBtn from 'components/NightModeToggleBtn';
+import TopControls from 'components/TopControls';
+import TodoListArea from 'components/TodoListArea';
+import CreateTodo from 'components/CreateTodo';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <Header />
+      <TopControls />
+      <TodoListArea />
       <FloatingAddBtn />
-      <NightModeToggleBtn />
+      <CreateTodo />
     </div>
   );
 };
